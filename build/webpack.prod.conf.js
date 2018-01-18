6614c0cb9119d91b5ac25eb2edcfe649c45cc982
lichenyang <lichenyang@allinmd.cn>
2018-01-18 15:57:56 +0800

    webpack.prod.conf.js 注释完成

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -1,16 +1,16 @@
-var path = require('path')
-var utils = require('./utils')
+var path = require('path') // path模块对路径的操作
+var utils = require('./utils') // 封装的工具
 var webpack = require('webpack')
-var config = require('../config')
+var config = require('../config') // 配置文件
 var merge = require('webpack-merge')
-var baseWebpackConfig = require('./webpack.base.conf')
-var CopyWebpackPlugin = require('copy-webpack-plugin')
-var HtmlWebpackPlugin = require('html-webpack-plugin')
-var ExtractTextPlugin = require('extract-text-webpack-plugin')
-var OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
-var glob = require('glob');
+var baseWebpackConfig = require('./webpack.base.conf') // webpack基础配置
+var CopyWebpackPlugin = require('copy-webpack-plugin') // 在webpack中拷贝文件和文件夹
+var HtmlWebpackPlugin = require('html-webpack-plugin') // html插件，生成的文件插入到html中
+var ExtractTextPlugin = require('extract-text-webpack-plugin') // 主要是为了抽离css样式,防止将样式打包在js中引起页面样式加载错乱的现象;
+var OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin') // 压缩css
+var glob = require('glob'); // glob是一个文件匹配包，就是用来根据指定样式或正则来匹配搜索文件的
 var env = config.build.env;
-var vConsolePlugin = require('vconsole-webpack-plugin');
+var vConsolePlugin = require('vconsole-webpack-plugin'); // vConsole 插件
 
 var webpackConfig = merge(baseWebpackConfig, {
   module: {
@@ -25,18 +25,23 @@ var webpackConfig = merge(baseWebpackConfig, {
     filename: utils.assetsPath('js/[name].[chunkhash].js'),
     chunkFilename: utils.assetsPath('js/[id].[chunkhash].js')
   },
+  // webpack插件位置，有固定的用法
+  // 1. 利用Plugin的初始方法并传入Plugin预设的参数进行初始化，生成一个实例。
+  // 2. 将此实例插入到webpack配置文件中的plugins参数（数组类型）里即可。
   plugins: [
     // http://vuejs.github.io/vue-loader/en/workflow/production.html
+    // DefinePlugin 是webpack 的内置插件，该插件可以在打包时候替换制定的变量
     new webpack.DefinePlugin({
       'process.env': env
     }),
+    // 压缩js
     new webpack.optimize.UglifyJsPlugin({
       compress: {
         warnings: false
       },
       sourceMap: config.build.productionSourceMap ? true : false
     }),
-    // extract css into its own file
+    // extract css into its own file 将css提取到自己的文件中
     new ExtractTextPlugin({
       filename: utils.assetsPath('css/[name].[contenthash].css')
     }),
@@ -48,7 +53,13 @@ var webpackConfig = merge(baseWebpackConfig, {
       }
     }),
     new webpack.optimize.ModuleConcatenationPlugin(),
-    // split vendor js into its own file
+    // split vendor js into its own file webpack分离js；根据需求分离js到不同的文件
+    // 如果文件是多入口的文件，可能存在，重复代码，把公共代码提取出来，又不会重复下载公共代码了
+    // （多个页面间会共享此文件的缓存）
+    // CommonsChunkPlugin的初始化常用参数有解析？
+    // name: 这个给公共代码的chunk唯一的标识
+    // filename，如何命名打包后生产的js文件，也是可以用上[name]、[hash]、[chunkhash]
+    // minChunks，公共代码的判断标准：某个js模块被多少个chunk加载了才算是公共代码
     new webpack.optimize.CommonsChunkPlugin({
       name: 'vendor',
       minChunks: function (module, count) {
@@ -100,7 +111,7 @@ if (config.build.productionGzip) {
     })
   )
 }
-
+// 开启 gzip 压缩 暂时没开启
 if (config.build.bundleAnalyzerReport) {
   var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
   webpackConfig.plugins.push(new BundleAnalyzerPlugin())
@@ -124,7 +135,6 @@ let pages = ((globalPath)=>{
 for (let entryName in pages) {
   let conf = {
     // 生成出来的html文件名
-    //Fixed by qiangkailiang on 20171204
     //若输出为首页，则输出至根目录下index.html
     filename: (entryName==="index"?"../"+entryName + '.html':entryName + '.html'),
     // 每个html的模版，这里多个页面使用同一个模版
@@ -140,7 +150,6 @@ for (let entryName in pages) {
     },
     chunks:[entryName,'manifest','vendor'],
     /*
-    * Fixed By Qiangkailiang on 20170806
     * 多页架构若不添加chunks将引入全部项目文件
     * entryName为页面唯一路径标识
     * 若有不匹配的资源文件将不被引入
